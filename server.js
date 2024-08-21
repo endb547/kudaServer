0105a822488d5f65af6d75df4bdbc05389c718fb
endb547 <endb547@gmail.com>
2024-08-21 17:39:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "endb547@gmail.com";
-//const pass = "jxlgucbkbfepomua";
+const pass = "jxlgucbkbfepomua";
 
 // Middleware
 app.use(express.json());
